14407387adf5b892122b95e643ea3a1486ff8076
fr1014 <38003556+fr1014@users.noreply.github.com>
2019-07-28 18:16:50 +0800

    Update BlurTransform.java

--- BlurTransform.java
+++ BlurTransform.java
@@ -1,3 +1,15 @@
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
+
+import androidx.annotation.NonNull;
+
+import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
+import com.bumptech.glide.load.resource.bitmap.BitmapTransformation;
+
 public class BlurTransform extends BitmapTransformation {
 
     private Context mContext;
