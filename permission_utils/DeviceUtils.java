16d85b6c132b637b6f0d586a8cfe3172567674a4
fr1014 <38003556+fr1014@users.noreply.github.com>
2019-08-12 14:16:49 +0800

    Create DeviceUtils.java

---
+++ permission_utils/DeviceUtils.java
@@ -0,0 +1,12 @@
+public class DeviceUtils {
+
+    /**
+     * 获取设备厂商
+     * 如 Xiaomi
+     * @return 设备厂商
+     */
+
+    public static String getManufacturer() {
+        return Build.MANUFACTURER;
+    }
+}
